d567f85c0663bb7b217951069951f01b9676ae0a
Dell <Dell@DESKTOP-OHID30O>
2024-01-04 12:56:05 +0100

    zadanie domowe dzień 4

---
+++ program.cs/program.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿var name = "Ewa";
+var sex = "Kobieta";
+var age = 30;
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if(name == "Ewa" && age == 30)
+{
+    Console.WriteLine("kobieta lat 30");
+}
+else if(sex == "mężczyna" && age < 18)
+{
+    Console.WriteLine("mezczyna niepelnoletni");
+}
